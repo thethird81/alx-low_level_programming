fffaf134e0dcd40ae1bf0c497d3e4e79527adb0f
tedo_1 <thethird81@gmail.com>
2022-07-01 14:17:19 -0700

    comming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - dora korpar
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+int ch;
+
+for (ch = 48; ch <= 57; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
