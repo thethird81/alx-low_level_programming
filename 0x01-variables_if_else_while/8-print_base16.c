0f7deb82df46b8277296fd324fb1b0dd90f628c5
tedo_1 <thethird81@gmail.com>
2022-07-01 14:28:43 -0700

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - dora korpar
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+{
+int i;
+char ch;
+for (i = 48; i <= 57; i++)
+{
+putchar(ch);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
