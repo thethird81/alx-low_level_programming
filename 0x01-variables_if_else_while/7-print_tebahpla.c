d44717ebf3d5a57fd657a90699968655597e6e40
tedo_1 <thethird81@gmail.com>
2022-07-01 14:19:54 -0700

    done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - dora korpar
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+int ch;
+
+for (ch = 122; ch <= 97; ch--)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
