fa8654e9cd2eff2aac8c8d06ff7b9461bfaf6cba
tedo_1 <thethird81@gmail.com>
2022-07-01 14:07:23 -0700

    uptoten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - dora korpar
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; ch < 10; ch++)
+{
+printf(" %d", i);
+}
+printf("\n");
+return (0);
+}
