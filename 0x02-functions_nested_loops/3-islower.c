ac419de9a3fdd7ef32694c4aa133fd41c0a36cfe
tedo_1 <thethird81@gmail.com>
2022-07-05 10:55:29 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks cas
+ * @c: - character
+ * Return: 0 if caps 1 if lower case
+ */
+
+int _islower(int c)
+{
+
+	if (c < 65)
+		return (1);
+	else
+		return (0);
+
+}
+
+
