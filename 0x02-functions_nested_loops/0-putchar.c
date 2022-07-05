37878a79e1ba34d7e3e76d0bbe1aa643a2d22ee2
tedo_1 <thethird81@gmail.com>
2022-07-05 04:56:08 -0700

    function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
+ * Return: 0
+ */
+int main(void)
+
+{
+
+	printf("_putchar\n");
+
+	return (0);
+
+}
+
+
