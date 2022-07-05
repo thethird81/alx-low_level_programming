4c452c4057bb8d446ddb96dd121e7fd05e91651e
tedo_1 <thethird81@gmail.com>
2022-07-05 11:42:52 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * @c: - character
+ * Return: 0 if caps 1 if lower case
+ */
+
+int _abs(int c)
+{
+
+	if (c < 0)
+		return ((-1) * c);
+	else
+		return (c);
+
+}
+
+
