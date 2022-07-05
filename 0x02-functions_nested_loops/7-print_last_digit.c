17982e2aed50449b8782c290521c875b68675cdb
tedo_1 <thethird81@gmail.com>
2022-07-05 11:48:53 -0700

    lastdigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - absolute value
+ * @c: - character
+ * Return: 0 if caps 1 if lower case
+ */
+
+int print_last_digit(int c)
+{
+return (c % 10);
+
+}
+
+
