699b3301b7b16c3b48d11267ae800e5f5e8e72c2
tedo_1 <thethird81@gmail.com>
2022-07-06 15:37:28 -0700

    new

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,16 +10,9 @@ int print_last_digit(int c)
 {
 int n;
 if (c < 0)
-	if (c != -2147483648)
-	{
 	c = -c;
 	n = c % 10;
 	_putchar(n + '0');
-	}
-	else
-	{
-	_putchar(8 + '0');
-	}
 	return (n);
 
 }
