a027528389faeee9d6338c03174ce0daf4415710
tedo_1 <thethird81@gmail.com>
2022-07-07 03:53:49 -0700

    morenum

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,26 +14,13 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	int k;
 
 	for (i = 0; i <= 14 ; i++)
 	{
-		j = i * 10;
-		k = j / 10;
-		if (j < 10)
+		for (j = 0 ; j < 15 ; j++)
 		{
-		_putchar(j + '0');
+			_putchar(j + '0');
 		}
-		if (j >= 10 && j < 100)
-		{
-			_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
-		}
-		else
-		{
-			_putchar(j / 100 + '0');
-			-putchar(k %  10 + '0');
-			_putchar(j % 10 + '0');
 	}
 	_putchar(10);
 
