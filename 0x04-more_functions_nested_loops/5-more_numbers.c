c8bb2e7f6c45145d3e7da9ee5416b772f500e4b0
tedo_1 <thethird81@gmail.com>
2022-07-07 03:42:46 -0700

    morenum

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+ * more_numbers - checks cas
+ * Return: 0 if caps 1 if lower case
+ */
+
+
+
+void more_numbers(void)
+
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 14 ; i++)
+	{
+		j = i;
+		if (j < 10)
+		{
+		_putchar(j + '0');
+		}
+		if (j >= 10)
+		{
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+	}
+	_putchar(10);
+
+}
