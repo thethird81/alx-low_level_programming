34e4326627e1a710397171bf213f43448269f42e
tedo_1 <thethird81@gmail.com>
2022-07-07 02:25:26 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+
+
+
+/**
+ *  _isupper - checks cas
+ * @c: - character
+ * Return: 0 if caps 1 if lower case
+ */
+
+
+
+int  _isupper(int c)
+
+{
+
+	if (c > 65 && c <= 90)
+	return (1);
+	else
+	return (0);
+}
