54225129abe1f2b7b09db49a5e72aa96f3ad6948
tedo_1 <thethird81@gmail.com>
2022-07-07 04:05:55 -0700

    new

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ * print_line - checks cas
+ * @n:num
+ * Return: 0 if caps 1 if lower case
+ */
+
+
+
+void print_line(int n)
+
+{
+
+	for (n = 0; n <= 14 ; n++)
+	{
+		_putchar('_');
+	}
+	}
+	_putchar(10);
+}
