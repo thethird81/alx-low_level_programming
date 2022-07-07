aed9c431ceb9f499b5777c54d6bd7f9a44f0188e
tedo_1 <thethird81@gmail.com>
2022-07-07 04:00:39 -0700

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,15 +19,8 @@ void more_numbers(void)
 	{
 		for (j = 0 ; j < 15 ; j++)
 		{
-			if (j < 10)
-			{
-			_putchar(j + '0');
-			}
-			else
-			{
-				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
-			}
+		_putchar(j + '0');
+
 		}
 	}
 	_putchar(10);
