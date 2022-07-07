3d1e1994c5692833d0a19a28491332ad045fa2fd
tedo_1 <thethird81@gmail.com>
2022-07-07 03:18:39 -0700

    list

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+
+/**
+ * print_numbers - checks cas
+ * Return: 0 if caps 1 if lower case
+ */
+
+
+
+void print_numbers(void)
+
+{
+
+	for (i = 0; i <= 9 ; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar(10);
+
+}
