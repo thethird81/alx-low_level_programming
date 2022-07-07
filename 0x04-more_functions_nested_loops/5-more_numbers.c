3132b50bb35e45d6695338584cf5ff92c7c3130b
tedo_1 <thethird81@gmail.com>
2022-07-07 03:48:39 -0700

    modulo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,19 +14,26 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i <= 14 ; i++)
 	{
-		j = i;
+		j = i * 10;
+		k = j / 10;
 		if (j < 10)
 		{
 		_putchar(j + '0');
 		}
-		if (j >= 10)
+		if (j >= 10 && j < 100)
 		{
 			_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 		}
+		else
+		{
+			_putchar(j / 100 + '0');
+			-putchar(k %  10 + '0');
+			_putchar(j % 10 + '0');
 	}
 	_putchar(10);
 
