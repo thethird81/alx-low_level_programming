01a588eab85e8332215ffb3f121d4678c81724db
tedo_1 <thethird81@gmail.com>
2022-07-07 02:44:32 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+
+/**
+ * _isdigit - checks cas
+ * @c: - character
+ * Return: 0 if caps 1 if lower case
+ */
+
+
+
+int  _isdigit(int c)
+
+{
+
+	if (c >= 0 && c <= 9)
+	return (1);
+	else
+	return (0);
+}
