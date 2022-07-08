89e34e42f585bd2908d1107388f6bdcf93fd3e99
tedo_1 <thethird81@gmail.com>
2022-07-08 04:02:34 -0700

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,32 +1,33 @@
 #include "main.h"
 
-
-
 /**
- * more_numbers - checks cas
- * Return: 0 if caps 1 if lower case
+ * more_numbers - a function that prints 10 times the numbers
+ * Return: 0-14 x10 followed by new line
  */
 
-
-
 void more_numbers(void)
-
 {
-	int i;
-	int j;
+	int a, b;
 
-	for (i = 0; i <= 10 ; i++)
+	for (b = 0; b < 10; b++)
 	{
-		for (j = 0 ; j <= 14 ; j++)
+		for (a = 0; a <= 14; a++)
 		{
-			if (j >= 10)
-			{
-				_putchar((j / 10) + '0');
-			}
-			_putchar((j % 10) + '0');
 
+			if (a >= 10)
+
+			{
+
+				_putchar((a / 10) + '0');
+
+			}
+
+			_putchar((a % 10) + '0');
 
 		}
+
 		_putchar('\n');
+
 	}
+
 }
