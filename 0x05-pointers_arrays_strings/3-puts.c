c6f850183f6be1d23cf02038c6c82e3242da7203
tedo_1 <thethird81@gmail.com>
2022-07-11 03:37:00 -0700

    new

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: string to be printed
+ *
+ * Return: void
+ **/
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		i++;
+	}
+	_putchar('\n');
+}
