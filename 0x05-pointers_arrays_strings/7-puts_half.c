0db6edaaed4d7b8df2975b8efa45852120acb03a
tedo_1 <thethird81@gmail.com>
2022-07-11 04:21:27 -0700

    new

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string to stdout
+ * @str: string to be printed
+ *
+ * Return: void
+ **/
+void puts_half(char *str)
+{
+	int length = _strlen(str);
+
+	if (length % 2 == 0)
+	{
+		length = length / 2;
+	}
+	else
+	{
+		length = (length + 1) / 2;
+	}
+	while (str[length] != '\0')
+	{
+		_putchar(str[length]);
+		length++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - counts the length of a string
+ * @s: string to  be counted
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0, i = 0;
+
+	while (s[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	return (length);
+}
