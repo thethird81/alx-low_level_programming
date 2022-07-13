9c17797479421a0e7407b41e64b0e0535e76f364
tedo_1 <thethird81@gmail.com>
2022-07-13 05:37:59 -0700

    good

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_buffer - print the content of @size bytes of the buffer pointed by @b
+ * @b: address of the buffer
+ * @size: size to be used
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i = 0, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (i < size)
+	{
+		if (i % 10 == 0)
+			printf("%08x: ", i);
+		for (j = i; j <= i + 9; j += 2)
+		{
+			if ((j < size) && ((j + 1) < size))
+				printf("%02x%02x ", b[j], b[j + 1]);
+			else
+			{
+				while (j <= i + 10)
+				{
+					printf("  ");
+					j++;
+				}
+				printf(" ");
+			}
+		}
+		for (j = i; j <= i + 9 && j < size; j++)
+		{
+			if (b[j] >= 32 && b[j] <= 126)
+				printf("%c", b[j]);
+			else
+				printf(".");
+		}
+		printf("\n");
+		i += 10;
+	}
+}
