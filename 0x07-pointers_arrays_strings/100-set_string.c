2442d72fe8dc85c2e66cb10b04ec6ba328228e7b
tedo_1 <thethird81@gmail.com>
2022-07-18 02:23:38 -0700

    new

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * set_string - sets the value of a pointer to a char
+  * @s: address of the pointer
+  * @to: new value
+  * Return: void
+  */
+void set_string(car **s, char *to)
+{
+	*s = to;
+}
