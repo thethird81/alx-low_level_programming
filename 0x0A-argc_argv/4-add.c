b0575a272eb8426b2beef3c2cde956ebbc09e7b8
tedo_1 <thethird81@gmail.com>
2022-07-26 11:02:19 -0700

    new

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+"main.h"
+
+/**
+ * main - ent#include ry point prints the sum of a variable amount of numbers
+* @argc: size of argv
+* @argv: array of pointers to command line args passed
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		while (--argc > 0)
+		{
+			if (is_valid(argv[argc]))
+				sum += atoi(argv[argc]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
+/**
+ * is_valid - checks if a number doesn't have any letters in it
+ * @s: address of string to be checked
+ *
+ * Return: 1 if valid, 0 otherwise
+ */
+int is_valid(char *s)
+{
+	while (*s)
+	{
+		if ((*s >= 'A' && *s <= 'Z') || (*s >= 'a' && *s <= 'z'))
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
