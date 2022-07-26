abdc526f7049c6cc87b1d0db66e37b7728736393
tedo_1 <thethird81@gmail.com>
2022-07-26 07:23:18 -0700

    new

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * * _strlen - length of a string
+ * * @s: input char
+ * * Return: length of a string
+ */
+ 
+  int _strl/en(char *s)
+  {
+  int l = 0;
+ 
+  while (*s != '\0')
+  {
+  s++;
+  l++;
+  }
+  return (l);
+  }
+ 
+
+/**
  * str_concat - Concat 2 strings.
  * @s1: string
  * @s2: string
