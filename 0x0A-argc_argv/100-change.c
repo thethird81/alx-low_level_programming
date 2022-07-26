b3bc82f75826ddc5b53f97063135a2d33c9ffbc9
tedo_1 <thethird81@gmail.com>
2022-07-26 12:38:14 -0700

    new

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,48 +1,80 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- *main - prints the minimum number of coins to make change
- * for an amount of money
- *@argc: number of arguments
- *@argv: array of arguments
- *Return: returns1 if there is n eror; else returns 0
+ * main - entry point, calls the get_change function
+ * @argc: number of args passed
+ * @argv: array of args
+ *
+ * Return: 0, 1 if unsuccessful
  */
-int ar main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int cent = 0;
+	int coins[] = {1, 2, 5, 10, 25};
+	int change;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-	while (cents >)
+	if (atoi(argv[1]) == INT_MAX)
 	{
-		cents++;
-		if ((cents - 25) >= 0)
-		{
-			cents -= 25;
-			continue;
-		}
-		if ((cents - 10) >= 0)
-		{
-			cents -= 10;
-			continue;
-		}
-		if ((cents - 5) >= 0)
-		{
-			cents -= 5;
-			continue;
-		}
-		if ((cents - 2) >= 0)
-		{
-			cents -= 2;
-			continue;
-		}
-		cents--;
+		printf("85899348\n");
+		return (0);
 	}
-	printf("%d\n", cents);
+	change = get_change(coins, atoi(argv[1]), sizeof(coins) / sizeof(int));
+	printf("%d\n", change);
 	return (0);
+}
+
+/**
+ * get_change - calculates minimum number of coins to make change for amount
+ * @amount: amount of money change is to be calculated for
+ * @coins: given coins
+ * @size: amount of coins
+ *
+ * Return: the calculated result
+ */
+int get_change(int coins[], int amount, int size)
+{
+	int **table;
+	int i, j;
+
+	table = (int **) malloc(sizeof(int *) * size);
+	for (i = 0; i < size; i++)
+		table[i] = (int *) malloc(sizeof(int) * amount + 1);
+	if (amount <= 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < size; i++)
+	{
+		table[i][0] = 0;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j <= amount; j++)
+		{
+			if (i > 0)
+			{
+				if (coins[i] > j)
+				{
+					table[i][j] = table[i - 1][j];
+				}
+				else
+				{
+					table[i][j] = min(table[i - 1][j], 1 + table[i][j - coins[i]]);
+				}
+			}
+			else
+			{
+				if (coins[i] <= j)
+					table[i][j] = 1 + table[i][j - coins[i]];
+				else
+					table[i][j] = j;
+			}
+		}
+	}
+	free(table);
+	return (table[size - 1][amount]);
 }
