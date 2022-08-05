1e65411cbfd33f6335dd362ccc5b05753b78b171
tedo_1 <thethird81@gmail.com>
2022-08-05 14:11:04 -0700

    new

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,16 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
 int _putchar(char c);
-char *create_array(unsigned int size, char);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 int len(char *str);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
-int num_words(char *str);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
